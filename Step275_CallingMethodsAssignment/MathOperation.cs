e059197fd7a9bab2291297ef82a55864fe0b22c8
Vinh Pham <viquph@gmail.com>
2021-09-27 14:54:41 -0400

    Added comments.

--- Step275_CallingMethodsAssignment/MathOperation.cs
+++ Step275_CallingMethodsAssignment/MathOperation.cs
@@ -6,26 +6,33 @@ using System.Threading.Tasks;
 
 namespace Step275_CallingMethodsAssignment
 {
+    // 1. Create a class.
+    // a. In that class, create three methods, each of which will take one integer parameter in and return an integer.
+    // b. The methods should do some math operation on the received parameter.
+    // c. Put this class in a separate.cs file in the application.
     public class MathOperation
     {
+        // Method to add userInput int by 10.
         public static int Add(int userInput)
         {
             int results = userInput + 10;
             return results;
         }
 
+        // Method to subtract userInput int by 10.
         public static int Subtract(int userInput)
         {
             int results = userInput - 10;
             return results;
         }
 
+        // Method to multiply userInput int by 10.
         public static int Multipy(int userInput)
         {
             int results = userInput * 10;
             return results;
         }
 
-        public int userInput { get; set; }
+        //public int userInput { get; set; }
     }
 }
--- Step275_CallingMethodsAssignment/Program.cs
+++ Step275_CallingMethodsAssignment/Program.cs
@@ -8,17 +8,22 @@ namespace Step275_CallingMethodsAssignment
 {
     class Program
     {
+        // 2. In the Main() program, ask the user what number they want to do the math operations on.
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, to which number would you like us to add, subtract, and multiply by 10?");
             int userInput = Convert.ToInt16(Console.ReadLine());
 
+            // 3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+            // calling Add() method to add the userInput int by 10, then assigning the results to the addResults int.
             int addResults = MathOperation.Add(userInput);
             Console.WriteLine($"{userInput} added by 10 is {addResults}.");
 
+            // calling Subtract() method to subtract the userInput int by 10, then assigning the results to the subtractResults int.
             int subtractResults = MathOperation.Subtract(userInput);
             Console.WriteLine($"{userInput} subtracted by 10 is {subtractResults}.");
 
+            // calling Multiply() method to multiply the userInput int by 10, then assigning the results to the multiplyResults int.
             int multiplyResults = MathOperation.Multipy(userInput);
             Console.WriteLine($"{userInput} multipled by 10 is {multiplyResults}.");
 
