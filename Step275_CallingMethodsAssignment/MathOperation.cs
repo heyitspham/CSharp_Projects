a8a791a7aa1f32397ee7e4038ec14c0c77dcb15f
Vinh Pham <viquph@gmail.com>
2021-09-27 14:11:25 -0400

    Step275 Calling Methods assignment.

---
+++ Step275_CallingMethodsAssignment/MathOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step275_CallingMethodsAssignment
+{
+    public class MathOperation
+    {
+        public static int Add(int userInput)
+        {
+            int results = userInput + 10;
+            return results;
+        }
+
+        public static int Subtract(int userInput)
+        {
+            int results = userInput - 10;
+            return results;
+        }
+
+        public static int Multipy(int userInput)
+        {
+            int results = userInput * 10;
+            return results;
+        }
+
+        public int userInput { get; set; }
+    }
+}
---
+++ Step275_CallingMethodsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step275_CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, to which number would you like us to add, subtract, and multiply by 10?");
+            int userInput = Convert.ToInt16(Console.ReadLine());
+
+            int addResults = MathOperation.Add(userInput);
+            Console.WriteLine($"{userInput} added by 10 is {addResults}.");
+
+            int subtractResults = MathOperation.Subtract(userInput);
+            Console.WriteLine($"{userInput} subtracted by 10 is {subtractResults}.");
+
+            int multiplyResults = MathOperation.Multipy(userInput);
+            Console.WriteLine($"{userInput} multipled by 10 is {multiplyResults}.");
+
+            Console.ReadLine();
+        }
+    }
+}
