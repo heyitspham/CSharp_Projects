33723a602f7e3dad1964835e405949f8dc8fc7af
Vinh Pham <viquph@gmail.com>
2021-09-28 01:06:25 -0400

    Step281 Method Class assignment.

---
+++ Step281_MethodClassAssignment/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281_MethodClassAssignment
+{
+    // 1. Create a class.
+    public class Math
+    {
+        // 1a. In that class, create a void method that takes two integers as parameters.
+        public void MathOp(int input01, int input02)
+        {
+            // 1b. Have the method do a math operation on the first integer
+            int result = input01 * 10;
+            Console.WriteLine($"The first input '{input01}' was multiplied by 10, and the result is: {result}");
+            
+            // 1c. and display the second integer to the screen.
+            Console.WriteLine($"The second input was: {input02}");
+        }
+    }
+}
---
+++ Step281_MethodClassAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281_MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Requested input, converted input to int16, and then assigned input to 'input01' int.
+            Console.WriteLine("Hi, Please input the 1st number:");
+            int input01 = Convert.ToInt16(Console.ReadLine());
+
+            // Requested input, converted input to int16, and then assigned input to 'input02' int.
+            Console.WriteLine("Please input the 2nd number:");
+            int input02 = Convert.ToInt16(Console.ReadLine());
+
+            // 2. In the Main() method of the console app, instantiate the class.
+            Math math = new Math();
+
+            // 3. Call the method in the class, passing in two numbers.
+            // 4. Call the method in the class, specifying the parameters by name.
+            math.MathOp(input01: input01, input02: input02);
+
+            Console.ReadLine();
+        }
+    }
+}
