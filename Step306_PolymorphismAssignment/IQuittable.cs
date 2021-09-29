f4e294e4fbbe72e923d01a115e85a7ccad15d747
Vinh Pham <viquph@gmail.com>
2021-09-29 13:46:29 -0400

    Step306 Polymorphism assignment.

---
+++ Step306_PolymorphismAssignment/IQuittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step306_PolymorphismAssignment
+{
+    // 1. Create an interface called IQuittable and
+    interface IQuittable
+    {
+        // 1a. have it define a void method called Quit().
+        void Quit();
+    }
+}
