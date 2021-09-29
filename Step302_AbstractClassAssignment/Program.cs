fbf35374bcc7e96096f54083b8a35b31fce56748
Vinh Pham <viquph@gmail.com>
2021-09-29 11:57:59 -0400

    Step302 Abstract Class assignment.

---
+++ Step302_AbstractClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step302_AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5. Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”.
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            
+            // 5a. Call the SayName() method on the object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
