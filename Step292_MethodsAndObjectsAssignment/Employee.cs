29560a5d84178ae5fae6cbb2c6ae964aa5daee4b
Vinh Pham <viquph@gmail.com>
2021-09-28 20:44:43 -0400

    Step292 Methods And Objects assignment.

---
+++ Step292_MethodsAndObjectsAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step292_MethodsAndObjectsAssignment
+{
+    // 3. Create another class called Employee and have it inherit from the Person class.
+    public class Employee : Person
+    {
+        // 3a. Give the Employee class a property called Id and have it be of data type int.
+        public int Id { get; set; }
+    }
+}
---
+++ Step292_MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step292_MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4. Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            // 5. Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
