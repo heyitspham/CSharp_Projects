f89308ddeb85ec0f47ff69d5dadbd696845a5110
Vinh Pham <viquph@gmail.com>
2021-10-01 17:45:24 -0400

    Step383 Constructor assignment.

---
+++ Step383_Constructor/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step383_Constructor
+{
+    public class Class1
+    {
+        public Class1(string name) : this(name, DateTime.Now)
+        {
+        }
+        public Class1(string name, DateTime datetime)
+        {
+            myName = name;
+            dateTime = datetime;            
+        }
+        public string myName { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
---
+++ Step383_Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step383_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Vinh Pham";
+
+            var datetime = new DateTime();
+            datetime = DateTime.Now;
+
+            Class1 class1 = new Class1(myName);
+
+            Console.WriteLine($"My name is {class1.myName},\nand the current time is {class1.dateTime}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// 1. Create a const variable.
+// 2. Create a variable using the keyword “var.”
+// 3. Chain two constructors together.
