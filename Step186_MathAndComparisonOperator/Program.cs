52d26d8c203f00b9bda357081653747432a18f9f
Vinh Pham <viquph@gmail.com>
2021-10-01 14:12:27 -0400

    Step186 Math & Comparison Operator assignment. Changed project name.

--- Step186_MathAndComparisonOperator/Program.cs
+++ Step186_MathAndComparisonOperator/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathAndComparisonOperatorAssignment
+namespace Step186_MathAndComparisonOperator
 {
     class Program
     {
