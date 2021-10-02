47067a464bacdc632b9422a8c0ab8dd6e2df78d6
Vinh Pham <viquph@gmail.com>
2021-10-01 22:05:37 -0400

    Added exception handlings.

---
+++ Step267_Casino_TwentyOneCardGame/FraudException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step267_Casino_TwentyOneCardGame
+{
+    public class FraudException : Exception
+    {
+        public FraudException()
+            : base() { }
+        public FraudException(string message)
+            : base(message) { }
+    }
+}
