9334f8d0b4feffc2f8839446946d4f075e73b946
Vinh Pham <viquph@gmail.com>
2021-10-04 16:58:12 -0400

    Step442 MVC tutorial

--- Step442_MVC_tutorial/Controllers/HomeController.cs
+++ Step442_MVC_tutorial/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Step442_MVC_tutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,54 @@ namespace Step442_MVC_tutorial.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\Administrator\source\repos\CSharp_WebApplications\Step442_MVC_tutorial\log.txt", text);
+
+            //Random rnd = new Random(10);
+            //int num = rnd.Next();
+            ////ViewBag.RandomNumber = num;
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //return View();
+
+            //return Content("hello");
+
+            //return RedirectToAction("Contact");
+
+            //return View("Contact");
+
+            //List<string> names = new List<string> { "Jesse", "Adam", "Brett"};
+            //return View(names);
+
+            //int number = 5;
+            //return View(number);
+
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Vinh";
+            user.LastName = "Pham";
+            user.Age = 32;
+            return View(user);
+
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
+            //throw new Exception("Invalid page");
+
             return View();
         }
 
-        public ActionResult Contact()
+        //public ActionResult Contact(int id = 0)
+        public ActionResult Contact(int id = 0)
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = id;
+            ViewBag.Message = id;
 
             return View();
         }
