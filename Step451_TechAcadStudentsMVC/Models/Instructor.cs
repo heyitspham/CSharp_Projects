273e9c9ed9ac89e964a75790eee52d9b8fa922e3
Vinh Pham <viquph@gmail.com>
2021-10-04 18:24:23 -0400

    Step451 MVC App

---
+++ Step451_TechAcadStudentsMVC/Models/Instructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Step451_TechAcadStudentsMVC.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
